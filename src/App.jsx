2b2233a65f286bd137fbf7e0f3c4b52cdf2a93cd
Moaz Mubaydin <mmubaydin@gmail.com>
2024-10-25 07:25:44 +0200

    added links and routes between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
 
-
 function App() {
-
   return (
     <div className="App relative z-20 pt-20">
       <Navbar />
-
-      <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
-      </div>
-
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route
+          path="/student/:studentId"
+          element={<StudentDetailsPage />}
+        ></Route>
+        <Route path="/userProfile" element={<UserProfilePage />}></Route>
+      </Routes>
+      <div className="pages"></div>
     </div>
   );
 }
